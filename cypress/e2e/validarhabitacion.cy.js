fcec140273470e5cde4203d0a0925fe5ae665e63
Cristian78904 <cristianbarriosrodrigues@gmail.com>
2026-07-20 11:59:56 -0300

    Verificación de Habitaciones del piso 2 al piso 5

--- cypress/e2e/validarhabitacion.cy.js
+++ cypress/e2e/validarhabitacion.cy.js
@@ -736,5 +736,344 @@ cy.visit('https://bedtrack-frontend-final-435l.vercel.app/')
     cy.get('.room-bed-action-btn').click()
     
     cy.get('.rba-avail').click()
+
+
+    })
+
+    it('Renderizar y verificar habitacion del 201 al 206 del piso 2', () => {
+
+    cy.visit('https://bedtrack-frontend-final-435l.vercel.app/')
+    cy.get('.login-card').should("be.visible")
+    cy.get('#role').should("be.visible")
+    cy.get('#role').select('enfermeria')
+    cy.get('input[name="email"]').should("be.visible")
+    cy.get('input[name="email"]').type('enfermeria@gmail.com')
+    cy.get('input[name="password"]').should("be.visible")
+    cy.get('input[name="password"]').type('1234')
+    cy.get('.btn-primary').should("be.visible")
+    cy.get('.btn-primary').click()
+    cy.get('.hamburger').click()
+
+
+    cy.get('[href="/habitaciones"]').click()
+    cy.get('.floor-tabs-wrap > :nth-child(2)').click()
+    cy.get('.page-wrapper').should("be.visible")
+    cy.get('.page-title').should("be.visible")
+    cy.get('.page-title').contains("Habitaciones por Piso")
+
+    cy.get('.page-subtitle').should("be.visible")
+    cy.get('.page-subtitle').contains("Seleccioná una habitación para gestionar sus camas · Piso 2")
+
+    cy.get('.floor-tabs-wrap > .active').should("be.visible")
+    cy.get('.active > .floor-tab-name').should("be.visible")
+    cy.get('.active > .floor-tab-name').contains("Piso 2")
+    cy.get('.active > .floor-tab-type').should("be.visible")
+    cy.get('.active > .floor-tab-type').contains("Compartida")
+
+    cy.get('.floor-tabs-wrap > :nth-child(2)').should("be.visible")
+    cy.get(':nth-child(2) > .floor-tab-name').should("be.visible")
+    cy.get(':nth-child(2) > .floor-tab-name').contains("Piso 2")
+    cy.get(':nth-child(2) > .floor-tab-type').should("be.visible")
+   
+
+    cy.get('.floor-tabs-wrap > :nth-child(3)').should("be.visible")
+    cy.get(':nth-child(3) > .floor-tab-name').should("be.visible")
+    
+    cy.get('.floor-info-banner').should("be.visible")
+    cy.get('.floor-info-icon').should("be.visible")
+    cy.get('.floor-info-title').should("be.visible")
+    cy.get('.floor-info-title').contains("Piso 2 · Compartida")
+    cy.get('.floor-info-desc').should("be.visible")
+    cy.get('.floor-info-desc').contains("6 habitaciones · 2 camas por habitación · 12 camas en total")
+
+    cy.get('.floor-info-chips').should("be.visible")
+    cy.get('.floor-info-chips > :nth-child(1)').should("be.visible")
+    cy.get('.floor-info-chips > :nth-child(2)').should("be.visible")
+    cy.get('.floor-info-chips > :nth-child(1) > span').contains("6 hab.")
+    cy.get('.floor-info-chips > :nth-child(2) > span').contains("12 camas")
+
+
+    cy.get('.stats-grid > :nth-child(1)').should("be.visible")
+    cy.get(':nth-child(1) > .stat-icon').should("be.visible")
+    cy.get(':nth-child(1) > .stat-info > .stat-label').should("be.visible")
+    cy.get(':nth-child(1) > .stat-info > .stat-value').should("be.visible")
+    cy.get(':nth-child(1) > .stat-info > .stat-value').contains("12")
+
+
+    cy.get('.stats-grid > :nth-child(2)').should("be.visible")
+    cy.get(':nth-child(2) > .stat-icon').should("be.visible")
+    cy.get(':nth-child(2) > .stat-info > .stat-label').should("be.visible")
+    cy.get(':nth-child(2) > .stat-info > .stat-value').should("be.visible")
+    cy.get(':nth-child(2) > .stat-info > .stat-value').contains("0")
+    
+
+    cy.get('.stats-grid > :nth-child(3)').should("be.visible")
+    cy.get(':nth-child(3) > .stat-icon').should("be.visible")
+    cy.get(':nth-child(3) > .stat-info > .stat-label').should("be.visible")
+    cy.get(':nth-child(3) > .stat-info > .stat-value').should("be.visible")
+    cy.get(':nth-child(3) > .stat-info > .stat-value').contains("0")
+
+
+    cy.get('.beds-header').should("be.visible")
+    cy.get('.beds-section-title').should("be.visible")
+    cy.get('.beds-section-title').contains("Habitaciones del Piso 2")
+
+ 
+    cy.get('.beds-count-badge').should("be.visible")
+    cy.get('.beds-count-badge').contains("6 habitaciones")
+
+
+    })
+    it('Renderizar y verificar habitacion del 301 al 306 del piso 3', () => {
+
+    cy.visit('https://bedtrack-frontend-final-435l.vercel.app/')
+    cy.get('.login-card').should("be.visible")
+    cy.get('#role').should("be.visible")
+    cy.get('#role').select('enfermeria')
+    cy.get('input[name="email"]').should("be.visible")
+    cy.get('input[name="email"]').type('enfermeria@gmail.com')
+    cy.get('input[name="password"]').should("be.visible")
+    cy.get('input[name="password"]').type('1234')
+    cy.get('.btn-primary').should("be.visible")
+    cy.get('.btn-primary').click()
+    cy.get('.hamburger').click()
+
+
+    cy.get('[href="/habitaciones"]').click()
+    cy.get('.floor-tabs-wrap > :nth-child(3)').click()
+    cy.get('.page-wrapper').should("be.visible")
+    cy.get('.page-title').should("be.visible")
+    cy.get('.page-title').contains("Habitaciones por Piso")
+
+    cy.get('.page-subtitle').should("be.visible")
+    cy.get('.page-subtitle').contains("Seleccioná una habitación para gestionar sus camas · Piso 3")
+
+    cy.get('.floor-tabs-wrap > .active').should("be.visible")
+    cy.get('.active > .floor-tab-name').should("be.visible")
+    cy.get('.active > .floor-tab-name').contains("Piso 3")
+    cy.get('.active > .floor-tab-type').should("be.visible")
+    cy.get('.active > .floor-tab-type').contains("Compartida")
+
+    cy.get('.floor-tabs-wrap > :nth-child(2)').should("be.visible")
+    cy.get(':nth-child(2) > .floor-tab-name').should("be.visible")
+    cy.get(':nth-child(3) > .floor-tab-name').contains("Piso 3")
+    cy.get(':nth-child(3) > .floor-tab-type').should("be.visible")
+   
+
+    cy.get('.floor-tabs-wrap > :nth-child(3)').should("be.visible")
+    cy.get(':nth-child(3) > .floor-tab-name').should("be.visible")
+    
+    cy.get('.floor-info-banner').should("be.visible")
+    cy.get('.floor-info-icon').should("be.visible")
+    cy.get('.floor-info-title').should("be.visible")
+    cy.get('.floor-info-title').contains("Piso 3 · Compartida")
+    cy.get('.floor-info-desc').should("be.visible")
+    cy.get('.floor-info-desc').contains("6 habitaciones · 2 camas por habitación · 12 camas en total")
+
+    cy.get('.floor-info-chips').should("be.visible")
+    cy.get('.floor-info-chips > :nth-child(1)').should("be.visible")
+    cy.get('.floor-info-chips > :nth-child(2)').should("be.visible")
+    cy.get('.floor-info-chips > :nth-child(1) > span').contains("6 hab.")
+    cy.get('.floor-info-chips > :nth-child(2) > span').contains("12 camas")
+
+
+    cy.get('.stats-grid > :nth-child(1)').should("be.visible")
+    cy.get(':nth-child(1) > .stat-icon').should("be.visible")
+    cy.get(':nth-child(1) > .stat-info > .stat-label').should("be.visible")
+    cy.get(':nth-child(1) > .stat-info > .stat-value').should("be.visible")
+    cy.get(':nth-child(1) > .stat-info > .stat-value').contains("12")
+
+
+    cy.get('.stats-grid > :nth-child(2)').should("be.visible")
+    cy.get(':nth-child(2) > .stat-icon').should("be.visible")
+    cy.get(':nth-child(2) > .stat-info > .stat-label').should("be.visible")
+    cy.get(':nth-child(2) > .stat-info > .stat-value').should("be.visible")
+    cy.get(':nth-child(2) > .stat-info > .stat-value').contains("0")
+    
+
+    cy.get('.stats-grid > :nth-child(3)').should("be.visible")
+    cy.get(':nth-child(3) > .stat-icon').should("be.visible")
+    cy.get(':nth-child(3) > .stat-info > .stat-label').should("be.visible")
+    cy.get(':nth-child(3) > .stat-info > .stat-value').should("be.visible")
+    cy.get(':nth-child(3) > .stat-info > .stat-value').contains("0")
+
+
+    cy.get('.beds-header').should("be.visible")
+    cy.get('.beds-section-title').should("be.visible")
+    cy.get('.beds-section-title').contains("Habitaciones del Piso 3")
+
+
+    cy.get('.beds-count-badge').should("be.visible")
+    cy.get('.beds-count-badge').contains("6 habitaciones")
+
+    })
+
+
+
+
+    it('Renderizar y verificar habitacion del 401 al 412 del piso 4 de terapia intensiva', () => {
+
+    cy.visit('https://bedtrack-frontend-final-435l.vercel.app/')
+    cy.get('.login-card').should("be.visible")
+    cy.get('#role').should("be.visible")
+    cy.get('#role').select('enfermeria')
+    cy.get('input[name="email"]').should("be.visible")
+    cy.get('input[name="email"]').type('enfermeria@gmail.com')
+    cy.get('input[name="password"]').should("be.visible")
+    cy.get('input[name="password"]').type('1234')
+    cy.get('.btn-primary').should("be.visible")
+    cy.get('.btn-primary').click()
+    cy.get('.hamburger').click()
+
+
+    cy.get('[href="/habitaciones"]').click()
+    cy.get('.floor-tabs-wrap > :nth-child(4)').click()
+    cy.get('.page-wrapper').should("be.visible")
+    cy.get('.page-title').should("be.visible")
+    cy.get('.page-title').contains("Habitaciones por Piso")
+
+    cy.get('.page-subtitle').should("be.visible")
+    cy.get('.page-subtitle').contains("Seleccioná una habitación para gestionar sus camas · Piso 4")
+
+    cy.get('.floor-tabs-wrap > .active').should("be.visible")
+    cy.get('.active > .floor-tab-name').should("be.visible")
+    cy.get('.active > .floor-tab-name').contains("Piso 4")
+    cy.get('.active > .floor-tab-type').should("be.visible")
+    cy.get('.active > .floor-tab-type').contains("Terapia Intensiva")
+
+    cy.get('.floor-tabs-wrap > :nth-child(2)').should("be.visible")
+    cy.get(':nth-child(4) > .floor-tab-name').should("be.visible")
+    cy.get(':nth-child(4) > .floor-tab-name').contains("Piso 4")
+    cy.get(':nth-child(4) > .floor-tab-type').should("be.visible")
+   
+
+    cy.get('.floor-tabs-wrap > :nth-child(4)').should("be.visible")
+    cy.get(':nth-child(4) > .floor-tab-name').should("be.visible")
+    
+    cy.get('.floor-info-banner').should("be.visible")
+    cy.get('.floor-info-icon').should("be.visible")
+    cy.get('.floor-info-title').should("be.visible")
+    cy.get('.floor-info-title').contains("Piso 4 · Terapia Intensiva")
+    cy.get('.floor-info-desc').should("be.visible")
+    cy.get('.floor-info-desc').contains("12 habitaciones · 1 cama por habitación · 12 camas en total")
+
+    cy.get('.floor-info-chips').should("be.visible")
+    cy.get('.floor-info-chips > :nth-child(1)').should("be.visible")
+    cy.get('.floor-info-chips > :nth-child(2)').should("be.visible")
+    cy.get('.floor-info-chips > :nth-child(1) > span').contains("12 hab.")
+    cy.get('.floor-info-chips > :nth-child(2) > span').contains("12 camas")
+
+
+    cy.get('.stats-grid > :nth-child(1)').should("be.visible")
+    cy.get(':nth-child(1) > .stat-icon').should("be.visible")
+    cy.get(':nth-child(1) > .stat-info > .stat-label').should("be.visible")
+    cy.get(':nth-child(1) > .stat-info > .stat-value').should("be.visible")
+    cy.get(':nth-child(1) > .stat-info > .stat-value').contains("12")
+
+
+    cy.get('.stats-grid > :nth-child(2)').should("be.visible")
+    cy.get(':nth-child(2) > .stat-icon').should("be.visible")
+    cy.get(':nth-child(2) > .stat-info > .stat-label').should("be.visible")
+    cy.get(':nth-child(2) > .stat-info > .stat-value').should("be.visible")
+    cy.get(':nth-child(2) > .stat-info > .stat-value').contains("0")
+    
+
+    cy.get('.stats-grid > :nth-child(3)').should("be.visible")
+    cy.get(':nth-child(3) > .stat-icon').should("be.visible")
+    cy.get(':nth-child(3) > .stat-info > .stat-label').should("be.visible")
+    cy.get(':nth-child(3) > .stat-info > .stat-value').should("be.visible")
+    cy.get(':nth-child(3) > .stat-info > .stat-value').contains("0")
+
+
+    cy.get('.beds-header').should("be.visible")
+    cy.get('.beds-section-title').should("be.visible")
+    cy.get('.beds-section-title').contains("Habitaciones del Piso 4")
+
+
+    cy.get('.beds-count-badge').should("be.visible")
+    cy.get('.beds-count-badge').contains("12 habitaciones")
+})
+    it('Renderizar y verificar habitacion del 501 al 512 del piso 5 de Aislamiento', () => {
+
+    cy.visit('https://bedtrack-frontend-final-435l.vercel.app/')
+    cy.get('.login-card').should("be.visible")
+    cy.get('#role').should("be.visible")
+    cy.get('#role').select('enfermeria')
+    cy.get('input[name="email"]').should("be.visible")
+    cy.get('input[name="email"]').type('enfermeria@gmail.com')
+    cy.get('input[name="password"]').should("be.visible")
+    cy.get('input[name="password"]').type('1234')
+    cy.get('.btn-primary').should("be.visible")
+    cy.get('.btn-primary').click()
+    cy.get('.hamburger').click()
+
+
+    cy.get('[href="/habitaciones"]').click()
+    cy.get('.floor-tabs-wrap > :nth-child(5)').click()
+    cy.get('.page-wrapper').should("be.visible")
+    cy.get('.page-title').should("be.visible")
+    cy.get('.page-title').contains("Habitaciones por Piso")
+
+    cy.get('.page-subtitle').should("be.visible")
+    cy.get('.page-subtitle').contains("Seleccioná una habitación para gestionar sus camas · Piso 5")
+
+    cy.get('.floor-tabs-wrap > .active').should("be.visible")
+    cy.get('.active > .floor-tab-name').should("be.visible")
+    cy.get('.active > .floor-tab-name').contains("Piso 5")
+    cy.get('.active > .floor-tab-type').should("be.visible")
+    cy.get('.active > .floor-tab-type').contains("Aislamiento")
+
+    cy.get('.floor-tabs-wrap > :nth-child(2)').should("be.visible")
+    cy.get(':nth-child(5) > .floor-tab-name').should("be.visible")
+    cy.get(':nth-child(5) > .floor-tab-name').contains("Piso 5")
+    cy.get(':nth-child(5) > .floor-tab-type').should("be.visible")
+   
+
+    cy.get('.floor-tabs-wrap > :nth-child(5)').should("be.visible")
+    cy.get(':nth-child(5) > .floor-tab-name').should("be.visible")
+    
+    cy.get('.floor-info-banner').should("be.visible")
+    cy.get('.floor-info-icon').should("be.visible")
+    cy.get('.floor-info-title').should("be.visible")
+    cy.get('.floor-info-title').contains("Piso 5 · Aislamiento")
+    cy.get('.floor-info-desc').should("be.visible")
+    cy.get('.floor-info-desc').contains("12 habitaciones · 1 cama por habitación · 12 camas en total")
+
+    cy.get('.floor-info-chips').should("be.visible")
+    cy.get('.floor-info-chips > :nth-child(1)').should("be.visible")
+    cy.get('.floor-info-chips > :nth-child(2)').should("be.visible")
+    cy.get('.floor-info-chips > :nth-child(1) > span').contains("12 hab.")
+    cy.get('.floor-info-chips > :nth-child(2) > span').contains("12 camas")
+
+
+    cy.get('.stats-grid > :nth-child(1)').should("be.visible")
+    cy.get(':nth-child(1) > .stat-icon').should("be.visible")
+    cy.get(':nth-child(1) > .stat-info > .stat-label').should("be.visible")
+    cy.get(':nth-child(1) > .stat-info > .stat-value').should("be.visible")
+    cy.get(':nth-child(1) > .stat-info > .stat-value').contains("12")
+
+
+    cy.get('.stats-grid > :nth-child(2)').should("be.visible")
+    cy.get(':nth-child(2) > .stat-icon').should("be.visible")
+    cy.get(':nth-child(2) > .stat-info > .stat-label').should("be.visible")
+    cy.get(':nth-child(2) > .stat-info > .stat-value').should("be.visible")
+    cy.get(':nth-child(2) > .stat-info > .stat-value').contains("0")
+    
+
+    cy.get('.stats-grid > :nth-child(3)').should("be.visible")
+    cy.get(':nth-child(3) > .stat-icon').should("be.visible")
+    cy.get(':nth-child(3) > .stat-info > .stat-label').should("be.visible")
+    cy.get(':nth-child(3) > .stat-info > .stat-value').should("be.visible")
+    cy.get(':nth-child(3) > .stat-info > .stat-value').contains("0")
+
+
+    cy.get('.beds-header').should("be.visible")
+    cy.get('.beds-section-title').should("be.visible")
+    cy.get('.beds-section-title').contains("Habitaciones del Piso 5")
+
+
+    cy.get('.beds-count-badge').should("be.visible")
+    cy.get('.beds-count-badge').contains("12 habitaciones")
+
 })
 })
